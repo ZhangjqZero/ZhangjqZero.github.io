2bc9ca98c2fcb2aae6be7b5900da31ff92279b31
ZhangjqZero <grzhang.jq@gmail.com>
2015-01-28 23:41:43 +0800

    Site updated: 2015-01-28 23:41:42

--- js/script.js
+++ js/script.js
@@ -1,36 +1,5 @@
 (function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
-
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
+  $('input[type=search]').removeAttr('results');
   // Share
   $('body').on('click', function(){
     $('.article-share-box.on').removeClass('on');
@@ -106,32 +75,13 @@
     $('.fancybox').fancybox();
   }
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
+  $('#nav-search-btn').on('click', function(){
+    $('#search-form-wrap').toggleClass('on');
+    $('#main-nav').toggleClass('off');
   });
 
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
+  // Mobile nav
+  $('#main-nav-toggle').on('click', function(){
+    $('#mobile-nav').toggleClass('on');
   });
 })(jQuery);
